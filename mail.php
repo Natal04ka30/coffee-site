379814a4fb6cc32d87d8f8194f8465e64b40c8c1
Natal04ka30 <106341548+Natal04ka30@users.noreply.github.com>
2023-10-26 16:56:09 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,63 @@
+
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$mail->SMTPDebug = 3;       
+
+$mail->isSMTP();                                      
+$mail->Host = 'smtp.mail.ru';  									
+$mail->SMTPAuth = true;                    
+$mail->Username = 'smakotina.natasha@mail.ru';
+$mail->Password = 'GFA3L5S1ahLB30iRwqsF'; 
+$mail->SMTPSecure = 'ssl';               
+$mail->Port = 465;
+
+$mail->setFrom('smakotina.natasha@mail.ru', 'COFFEE SITE'); 
+$mail->addAddress('smakotina.natasha@mail.ru');       
+$mail->isHTML(true);
+$mail->Subject = 'Заявка с сайта Coffee';
+//проверяем поля формы
+
+function clear_data($val){
+	$val = trim($val);
+	$val = stripslashes($val);
+	$val = strip_tags($val);
+	$val = htmlspecialchars($val);
+	return $val;
+}
+var_dump($_POST);
+$name = clear_data($_POST['name']);
+$email = clear_data($_POST['email']);
+$message = clear_data($_POST['message']);
+
+//тело письма
+$body = '<h1>Сообщение с сайта</h1>';
+
+if(!empty($name)) {
+	$body.='<p><strong>Имя:</strong> '.$name.'</p>';
+}
+if(!empty($email)) {
+	$body.='<p><strong>E-mail:</strong> '.$email.'</p>';
+}
+if(!empty($message)) {
+$body.='<p><strong>Сообщение:</strong> '.$message.'</p>';
+}
+
+$mail->Body=$body;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+	echo 'Данные отправлены';
+} else {
+	echo 'Произошла ошибка';
+}
+?>
